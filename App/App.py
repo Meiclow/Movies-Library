2f4ee4dda6ffacca5b7ceda5018c2e37f6e63303
Meiclow <56319233+Meiclow@users.noreply.github.com>
2020-05-28 20:12:17 +0200

    login

--- App/App.py
+++ App/App.py
@@ -6,3 +6,17 @@ db = client["movies_library"]
 movies_col = db["movies"]
 reviews_col = db["reviews"]
 users_col = db["users"]
+
+
+def login(name, password):
+    user = users_col.find_one({"name": name})
+    print(user)
+    if not user:
+        print("wrong username or password")
+        return None
+    if user["password"] != password:
+        print("wrong username or password")
+        return None
+    return user["_id"]
+
+print(login("David Gebhard", "2Cbgopm2"))
